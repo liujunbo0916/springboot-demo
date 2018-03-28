38e50e0493deebde54a5925c7afddc7f62948ad3
ljb0916 <1452884372@qq.com>
2018-03-28 21:39:33 +0800

    集成完毕

---
+++ src/main/java/com/liujunbo/springbootdemo/groovy/GrammarTest.groovy
@@ -0,0 +1,36 @@
+package com.liujunbo.springbootdemo.groovy
+
+
+    def value = 1
+    value = value + 1
+    println(value)
+
+   def m = 5
+   def j = 2
+   print  12*m /j
+
+  def  age = 25
+  println "My age is ${age}"
+println 'My age is ${age}'
+println """My age is ${age} """
+println "My age is \${age}"
+
+  def greeting  = 'hello word'
+  println  greeting - 'o word'
+  println greeting.size()
+  println greeting.count('o')
+  println greeting.intern()
+  println greeting.center(20)
+  println greeting.center(20,'@')
+
+
+
+
+
+
+
+
+
+
+
+
