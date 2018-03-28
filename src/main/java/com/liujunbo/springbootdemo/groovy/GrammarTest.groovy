7bd4e7ee1d5e9b5da19e96773f42baed77413fe2
ljb0916 <1452884372@qq.com>
2018-03-28 22:22:26 +0800

    集成完毕

--- src/main/java/com/liujunbo/springbootdemo/groovy/GrammarTest.groovy
+++ src/main/java/com/liujunbo/springbootdemo/groovy/GrammarTest.groovy
@@ -23,6 +23,16 @@ println "My age is \${age}"
   println greeting.center(20)
   println greeting.center(20,'@')
 
+ def list1 = [34,65,76,87,1,2,3]
+ def list2 = list1.getAt([2,3,5,6]) //返回 列表索引对应值的列表
+ def list3 = list1.getAt(1..3);
+ print list2
+ print list3
+ def map1 = [4:[2],5:[3,4],6:[7,8]]
+println map1[5]
+
+
+
 
 
 
--- src/test/java/com/liujunbo/springbootdemo/SpringbootDemoApplicationTests.java
+++ src/test/java/com/liujunbo/springbootdemo/SpringbootDemoApplicationTests.java
@@ -5,12 +5,27 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootDemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	public static void main(String[] args) {
+
+		Map<String,String> map = new HashMap<>();
+		map.put("1","11");
+		map.put("2","22");
+		map.put("3","33");
+		map.put("4","44");
+		map.forEach((key,value)->System.out.println("key:"+key+" value:"+value));
+
+
 	}
 
 }
